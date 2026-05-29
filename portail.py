4c1c6fa47520365853e7c7f9fe59e7b4a2f1d2e1
atrcrege-a11y <atrcrege@gmail.com>
2026-05-29 07:41:04 +0200

    Portail v11.1 - Mise a jour v11.1

--- portail.py
+++ portail.py
@@ -5,7 +5,7 @@ import os, sys, subprocess, threading, webbrowser, time, json, tempfile
 import urllib.request as urlreq
 from flask import Flask, render_template_string, jsonify, request, make_response
 
-VERSION_LOCALE = "11.0"
+VERSION_LOCALE = "11.1"
 VERSION_JSON_URL = "https://raw.githubusercontent.com/atrcrege-a11y/Portail-LREGE/main/version.json"
 
 app = Flask(__name__)
